96db94c398c3ea8cff402d24b7c6b3c4b5e2b44e
Seth Hammell <sethjhammell@gmail.com>
2021-10-30 03:03:59 -0400

    Fixed bug where renamed videos were interpreted as removed

--- FindRemovedSongs.py
+++ FindRemovedSongs.py
@@ -8,12 +8,21 @@ def findRemovedSongs(playlists_past, playlists_current):
     try:
         for playlist in range(len(playlists_past)):
             if (playlist >= len(playlists_current)):
-              break
+                break
             removed_songs_playlist = []
 
-            for video in playlists_past[playlist].videos:
+            for i, video in enumerate(playlists_past[playlist].videos):
                 if (not (video[0], video[1]) in playlists_current[playlist].videos):
-                    removed_songs_playlist.append(video)
+                    if (i != 0 and i != len(playlists_past[playlist].videos) - 1):
+                        prev_video = playlists_current[playlist].videos[i - 1]
+                        next_video = playlists_current[playlist].videos[i + 1]
+                        if prev_video in playlists_current[playlist].videos and next_video in playlists_current[playlist].videos:
+                            prev_video_index = playlists_current[playlist].videos.index(prev_video)
+                            next_video_index = playlists_current[playlist].videos.index(next_video)
+                            if next_video_index - prev_video_index != 2:
+                                removed_songs_playlist.append(video)
+                    else:
+                      removed_songs_playlist.append(video)
 
             if (removed_songs_playlist != []):
                 removed_songs_all.append({ playlists_current[playlist].name : removed_songs_playlist })
