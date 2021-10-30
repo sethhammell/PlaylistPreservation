2cad97c6743efa6d17addf2b17b6cbb23c49b63b
Seth Hammell <sethjhammell@gmail.com>
2021-10-30 03:23:43 -0400

    fixed edge case where renamed video is first or last video in playlist

--- FindRemovedSongs.py
+++ FindRemovedSongs.py
@@ -2,7 +2,6 @@ import logging
 from EmailResults import emailError
 
 def findRemovedSongs(playlists_past, playlists_current):
-
     removed_songs_all = []
 
     try:
@@ -11,18 +10,14 @@ def findRemovedSongs(playlists_past, playlists_current):
                 break
             removed_songs_playlist = []
 
-            for i, video in enumerate(playlists_past[playlist].videos):
+            for video in playlists_past[playlist].videos:
                 if (not (video[0], video[1]) in playlists_current[playlist].videos):
-                    if (i != 0 and i != len(playlists_past[playlist].videos) - 1):
-                        prev_video = playlists_current[playlist].videos[i - 1]
-                        next_video = playlists_current[playlist].videos[i + 1]
-                        if prev_video in playlists_current[playlist].videos and next_video in playlists_current[playlist].videos:
-                            prev_video_index = playlists_current[playlist].videos.index(prev_video)
-                            next_video_index = playlists_current[playlist].videos.index(next_video)
-                            if next_video_index - prev_video_index != 2:
-                                removed_songs_playlist.append(video)
-                    else:
-                      removed_songs_playlist.append(video)
+                    removed = True
+                    for current_video in playlists_current[playlist].videos:
+                        if video[1] == current_video[1]:
+                            removed = False
+                    if removed:
+                        removed_songs_playlist.append(video)
 
             if (removed_songs_playlist != []):
                 removed_songs_all.append({ playlists_current[playlist].name : removed_songs_playlist })
