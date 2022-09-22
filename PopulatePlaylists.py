caf42351a08aa893a161c22e5fd5a6eda165c4b0
Helix48 <sethjhammell@gmail.com>
2022-09-21 20:35:12 -0400

    use dynamic log path

--- PopulatePlaylists.py
+++ PopulatePlaylists.py
@@ -5,19 +5,19 @@ import shutil
 
 logging = True
 
+# Playlists must be in alphabetical order in json
+playlists_json = open("data.json")
+playlists_data = json.load(playlists_json)
+
 def ResetLogDirectory():
   directory = "TodaysLogs"
-  parent_dir = "C:/Users/sethj/OneDrive/Desktop/Programming/PlaylistPreservation/"
+  parent_dir = playlists_data["logPath"]
   path = os.path.join(parent_dir, directory)
   shutil.rmtree(path)
   os.mkdir(path)
 
 def populatePlaylists():
     playlists = []
-
-    # Playlists must be in alphabetical order in json
-    playlists_json = open("data.json")
-    playlists_data = json.load(playlists_json)
 
     if logging:
         ResetLogDirectory()
