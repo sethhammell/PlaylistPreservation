ef08840e94c9f8b30b37b0a702c194b874db34ef
Helix48 <sethjhammell@gmail.com>
2023-04-29 04:29:19 -0400

    added functionality to find all removed songs

--- FirebaseContext.py
+++ FirebaseContext.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 import firebase_admin
 import firebase_admin.credentials as credentials
 import firebase_admin.db as db
@@ -7,36 +7,42 @@ import logging
 from Playlist import Playlist
 from EmailResults import emailError
 
+all_removed_songs = False
+format_change_date = date(2021, 9, 2)
+
 if not firebase_admin._apps:
     firebase_json = open("data.json")
     firebase_data = json.load(firebase_json)
 
-    cred = credentials.Certificate(firebase_data["credentials"]) 
+    cred = credentials.Certificate(firebase_data["credentials"])
 
     try:
-        default_app = firebase_admin.initialize_app(cred, {"databaseURL": firebase_data["databaseURL"]})
+        default_app = firebase_admin.initialize_app(
+            cred, {"databaseURL": firebase_data["databaseURL"]})
     except Exception as ex:
         error_msg = "Failed to connect to database"
         logging.exception(error_msg)
         emailError(error_msg, ex)
 
+
 def postPlaylistsToFirebase(playlists_current):
-    ref = db.reference("playlists", app = default_app)
+    ref = db.reference("playlists", app=default_app)
     id = datetime.now().date()
     playlists_ref_current = ref.child(str(id))
 
     try:
         for playlist in playlists_current:
             playlists_ref_current.update({
-                playlist.name : json.dumps(playlist.videos)
+                playlist.name: json.dumps(playlist.videos)
             })
     except Exception as ex:
         error_msg = "Failed to post data"
         logging.exception(error_msg)
         emailError(error_msg, ex)
 
+
 def readPastPlaylistsFromFirebase():
-    ref = db.reference("playlists", app = default_app)
+    ref = db.reference("playlists", app=default_app)
     id = datetime.now().date() + timedelta(days=-1)
 
     try:
@@ -44,15 +50,43 @@ def readPastPlaylistsFromFirebase():
         playlists_ref_past = ref.child(str(id))
         playlists_past_json = playlists_ref_past.get(etag=True)
 
-        i = 0
-        while ((playlists_past_json[0] == None) and i < 365):
-            id += timedelta(days=-1)
-            playlists_ref_past = ref.child(str(id))
-            playlists_past_json = playlists_ref_past.get(etag=True)
-            i += 1
+        if all_removed_songs:
+            all_songs = {}
+            while (id > format_change_date):
+                print(id)
+                id += timedelta(days=-1)
+                playlists_ref_past = ref.child(str(id))
+                playlists_past_json = playlists_ref_past.get(etag=True)
 
-        for playlist in playlists_past_json[0]:
-            playlists_past.append(Playlist(playlist, "None", json.loads(playlists_past_json[0][playlist])))
+                if playlists_past_json[0] != None:
+                    if not len(all_songs):
+                        for playlist in playlists_past_json[0]:
+                            all_songs[playlist] = {}
+                    for playlist in playlists_past_json[0]:
+                        new_songs = json.loads(
+                            playlists_past_json[0][playlist])
+
+                        for song in new_songs:
+                            if song[1] not in all_songs[playlist]:
+                                all_songs[playlist][song[1]] = song[0]
+
+            for playlist in all_songs:
+                new_playlist = [(name, id)
+                                for id, name in all_songs[playlist].items()]
+                playlists_past.append(
+                    Playlist(playlist, "None", new_playlist))
+
+        else:
+            i = 0
+            while ((playlists_past_json[0] == None) and i < 365):
+                id += timedelta(days=-1)
+                playlists_ref_past = ref.child(str(id))
+                playlists_past_json = playlists_ref_past.get(etag=True)
+                i += 1
+
+            for playlist in playlists_past_json[0]:
+                playlists_past.append(
+                    Playlist(playlist, "None", json.loads(playlists_past_json[0][playlist])))
     except Exception as ex:
         error_msg = "Failed to read data"
         logging.exception(error_msg)
